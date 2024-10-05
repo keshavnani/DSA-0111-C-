e2ae1a5d7d3a27ce5217eae925ed742b7a911055
keshavnani <134251499+keshavnani@users.noreply.github.com>
2024-10-05 10:39:36 +0530

    Add files via upload

---
+++ c++/positive or negetive.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter an interger: ";
+	cin >> n;
+	if (n>0)
+	{
+		cout<< n <<" it is positive "<<endl;
+	}
+	else
+	{
+		cout<< n <<" it is negetive "<<endl;
+	}
+	return 0;
+}
